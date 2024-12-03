14de657e58a370e419c48b8273d5316ffc963428
Pariharx7 <parihar.peace@gmail.com>
2024-12-03 19:01:53 +0530

    Added user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,7 +1,25 @@
 const express = require("express");
-
+const zod = require("zod");
+const { User } = require("../db");
 const router = express.Router();
 
-router.post("/signup")
+const signupSchema = zod.object({
+    username: zod.string(),
+    password: zod.string(),
+    firstName: zod.string(),
+    password: zod.string()
+})
+
+router.post("/signup", (req,res) => {
+    const body = req.body;
+    const {success} = signupSchema.safeParse(req.body);
+    if(!success){
+        return res.json({
+            message: "Email already taken / Incorrect inputs"
+        })
+    }
+
+    const user = User.findOne()
+})
 
 module.exports = router;
