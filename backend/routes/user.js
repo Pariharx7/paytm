83b9991f08a2118fd8a2fa07b4e0c0431451420e
Pariharx7 <parihar.peace@gmail.com>
2024-12-05 19:47:51 +0530

    Added user.js

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -49,11 +49,69 @@ router.post("/signup", async (req,res) => {
     const token = jwt.sign({
         userId
     }, JWT_SECRET)
-    
+
     res.json({
         message: "User created successfully",
         token: token
     })
 })
+
+
+const signinBody = zod.object({
+    username: zod.string().email(),
+    password: zod.string()
+})
+
+router.post("/signin", async(req, res) => {
+    const { success } = signinBody.safeParse(req.body)
+    if(!success){
+        return res.status(411).json({
+            message: "Email already taken/ Incorrect Inputs"
+        })
+    }
+
+    const user = await User.findOne({
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    if(user){
+        const token = jwt.sign({
+            userId: user._id
+        }, JWT_SECRET);
+
+        res.json({
+            token: token
+        })
+        return;
+    }
+
+    res.status(411).json({
+        message: "Error while logging in"
+    })
+})
+
+    const updateBody = zod.object({
+        password: zod.string().optional(),
+        firstName: zod.string().optional(),
+        lastName: zod.string.optional(),
+    })
+
+    router.put("/", authMiddleware, async(req, res) => {
+        const { success } = updateBody.safeParse(req.body)
+        if(!success){
+            res.status(411).json({
+                message: "Error while updating information"
+            })
+        }
+
+        await User.updateOne(req.body, {
+            id: req.userId
+        })
+
+        res.json({
+            message: "Updated successfully"
+        })
+    })
 
 module.exports = router;
