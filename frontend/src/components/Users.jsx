0e63a1c5446d935ca819b445d112ee107f14d857
Pariharx7 <parihar.peace@gmail.com>
2024-12-10 18:59:49 +0530

    Added Users.jsx

---
+++ frontend/src/components/Users.jsx
@@ -0,0 +1,31 @@
+import { useEffect,useState } from "react";
+import { Button } from "./Button"
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
+import { response } from "express";
+
+export const Users = () => {
+    const [users, setUsers] = useState([]);
+    const [filter, setFilter] = useState("");
+
+    useEffect(() => {
+        axios.get("https://localhost:3000/api/v1/user/bulk?filter=" + filter)
+            .then(response => {
+                setUsers(response.data.user)
+            })
+    }, [filter])
+
+    return <>
+        <div className="font-bold mt-6 text-lg">
+            Users
+        </div>
+        <div className="my-2">
+            <input onChange={(e) => {
+                setFilter(e.target.value)
+            }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200" ></input>
+        </div>
+        <div>
+            {users.map(user => <User user={user} />)}
+        </div>
+    </>
+}
